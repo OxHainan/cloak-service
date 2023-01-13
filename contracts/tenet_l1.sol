f63c8cbfcc07eb287ee05676b53ae96cce94f207
JUN <yingjun@oxhainan.org>
2023-01-13 18:08:46 +0800

    Update tenet_l1.sol

--- contracts/tenet_l1.sol
+++ contracts/tenet_l1.sol
@@ -1,16 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0
- pragma solidity >=0.8.7;
+pragma solidity >=0.8.7;
 
- import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface TenetL2 {
+    function mint(
+        address token,
+        address to,
+        uint256 amount
+    ) external;
+}
 
 interface IERC20Minimal {
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 }
 
 contract Tenet is Ownable {
-
     struct App {
         bool isResgister;
         string id;
@@ -38,39 +53,78 @@ contract Tenet is Ownable {
     mapping(address => bool) tokenRegistered;
     mapping(string => mapping(uint256 => Round)) rounds;
 
-    event Deposit(
-            address token, address user, uint256 amount
-        );
+    event Deposit(address token, address user, uint256 amount);
+    event Dispatch(bytes payload);
+
     function deposit(address token, uint256 amount) external {
         require(tokenRegistered[token], "Token not registered");
         (bool success, bytes memory data) = token.call(
-            abi.encodeWithSelector(IERC20Minimal.transferFrom.selector, msg.sender, address(this), amount)
+            abi.encodeWithSelector(
+                IERC20Minimal.transferFrom.selector,
+                msg.sender,
+                address(this),
+                amount
+            )
         );
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "Deposit failed");
-        emit Deposit(token, msg.sender, amount);
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "Deposit failed"
+        );
+
+        emit Dispatch(
+            abi.encodeWithSelector(
+                TenetL2.mint.selector,
+                token,
+                msg.sender,
+                amount
+            )
+        );
+        // emit Deposit(token, msg.sender, amount);
     }
 
     event Withdraw(address token, address user, uint256 amount);
-    function withdrawl(address token, address user, uint256 amount) external { // TODO: onlyAppAdmin
+
+    function withdraw(
+        address token,
+        address user,
+        uint256 amount
+    ) external {
+        // TODO: onlyAppAdmin
         require(tokenRegistered[token], "Token not registered");
-        require(balance(token) >= amount, "Token balance lower than the value requested");
-        (bool success, bytes memory data) = token.call(
-            abi.encodeWithSelector(IERC20Minimal.transfer.selector, user, amount)
+        require(
+            balance(token) >= amount,
+            "Token balance lower than the value requested"
         );
-        require(success && (data.length == 0 || abi.decode(data, (bool))), "Withdral failed");
-        emit Withdraw(token, msg.sender, amount);
+        (bool success, bytes memory data) = token.call(
+            abi.encodeWithSelector(
+                IERC20Minimal.transfer.selector,
+                user,
+                amount
+            )
+        );
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            "Withdral failed"
+        );
+        emit Withdraw(token, user, amount);
     }
 
-    function balance(address tokenAddr) internal view returns(uint256) {
+    function balance(address tokenAddr) internal view returns (uint256) {
         (bool success, bytes memory data) = tokenAddr.staticcall(
-            abi.encodeWithSelector(IERC20Minimal.balanceOf.selector, address(this))
+            abi.encodeWithSelector(
+                IERC20Minimal.balanceOf.selector,
+                address(this)
+            )
         );
         require(success && data.length >= 32);
         return abi.decode(data, (uint256));
     }
 
     function registerToken(address tokenAddr) external onlyOwner {
-        require(!tokenRegistered[tokenAddr], "Token address already registered");
+        require(
+            !tokenRegistered[tokenAddr],
+            "Token address already registered"
+        );
         tokenRegistered[tokenAddr] = true;
     }
 
@@ -83,9 +137,13 @@ contract Tenet is Ownable {
     }
 
     function registerNode(
-        address id, string calldata ip, bytes32  mrenclave, uint256 version,
-        bytes32 publicKey, bytes calldata report
-     ) external onlyAppAdmin {
+        address id,
+        string calldata ip,
+        bytes32 mrenclave,
+        uint256 version,
+        bytes32 publicKey,
+        bytes calldata report
+    ) external onlyAppAdmin {
         require(!nodes[id].isResgister, "Node already registered");
         nodes[id].id = id;
         nodes[id].ip = ip;
@@ -97,9 +155,14 @@ contract Tenet is Ownable {
     }
 
     modifier onlyAppAdmin() {
-        require(bytes(admin2AppId[msg.sender]).length > 0, "App not registered or permission denied");
-        require(apps[admin2AppId[msg.sender]].admin == msg.sender, "Permision denied");
+        require(
+            bytes(admin2AppId[msg.sender]).length > 0,
+            "App not registered or permission denied"
+        );
+        require(
+            apps[admin2AppId[msg.sender]].admin == msg.sender,
+            "Permision denied"
+        );
         _;
     }
-
 }
